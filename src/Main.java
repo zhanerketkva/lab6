5850aa401a018b76b0116eba4c3a10d6f8ef61b5
lab1 <zhanerketkva7@gmail.com>
2023-05-31 23:20:48 +0600

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numOfVertices = 5;
+        WeightedGraph<Integer> graph = new WeightedGraph<>(numOfVertices);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 4);
+
+        System.out.println("Graph:");
+        graph.printGraph();
+
+        System.out.println("BFS:");
+        Search bfs = new BreadthFirstSearch(graph);
+        bfs.search(0);
+
+        System.out.println("\nDijkstra:");
+        Search dijkstra = new DijkstraSearch(graph);
+        dijkstra.search(0);
     }
-}
