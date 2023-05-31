681c79bdbe123ffe1181c9d34971eb00bf132951
lab1 <zhanerketkva7@gmail.com>
2023-05-31 23:17:36 +0600

    Vertex

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,2 +1,16 @@
-public class Vertex {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Vertex<T> { //vertex object with a value of type T
+    private T value;
+
+    public Vertex(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
 }
