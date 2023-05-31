e05c2bb46c83c14cf2d6909dbb5f060f92b23a2d
lab1 <zhanerketkva7@gmail.com>
2023-05-31 23:19:07 +0600

    VertexDistancePair

--- src/VertexDistancePair.java
+++ src/VertexDistancePair.java
@@ -1,2 +1,22 @@
-public class VertexDistancePair {
+public class VertexDistancePair implements Comparable<VertexDistancePair> {
+    private int vertex;
+    private double distance;
+
+    public VertexDistancePair(int vertex, double distance) {
+        this.vertex = vertex;
+        this.distance = distance;
+    }
+
+    public int getVertex() { //returns the vertex value of the pair
+        return vertex;
+    }
+
+    public double getDistance() { //returns the distance value of the pair
+        return distance;
+    }
+
+    @Override
+    public int compareTo(VertexDistancePair other) { //implements the compareTo method from the Comparable interface
+        return Double.compare(distance, other.distance);
+    }
 }
